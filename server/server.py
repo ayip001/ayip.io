dcccc0c5223c6e23d8cab744ef3e1bc848915dd2
Angus Yip <ayip002@gmail.com>
2017-11-10 15:09:52 -0800

    part of hello

--- server/server.py
+++ server/server.py
@@ -1,7 +1,19 @@
 # server.py
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static/templates")
+
+def get_hello():
+    greeting_list = [
+        'Ciao',
+        'Hei',
+        'Salut',
+        'Hola',
+        'Hallo',
+        'Hej'
+    ]
+    return random.choice(greeting_list)
 
 @app.route("/")
 def index():
@@ -9,7 +21,7 @@ def index():
 
 @app.route("/hello")
 def hello():
-    return "Hello World!"
+    return get_hello()
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
--- static/js/index.jsx
+++ static/js/index.jsx
@@ -4,4 +4,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import Hello from "./Hello";
+import { PageHeader } from "react-bootstrap";
+
 ReactDOM.render(<App />, document.getElementById("content"));
+
+render () {
+    return (
+        <PageHeader>
+            <div className="header-contents">
+                <Hello name="Rimini" />
+            </div>
+        </PageHeader>
+    );
+}
