f9d3be1ef65dbf43631a19b3456852fdfbe85fd8
Angus Yip <ayip002@gmail.com>
2017-12-16 18:31:42 -0800

    Add barebone post api functionality

--- src/server/api/posts.py
+++ src/server/api/posts.py
@@ -1,10 +1,22 @@
 # ./src/server/api/posts.py
-from flask import jsonify
+from flask import jsonify, abort
 from flask_restful import Resource
 from google.cloud import datastore
 
 client = datastore.Client()
 
-class PostsAPI(Resource):
-    def get(self, id):
-        return jsonify( { 'it': 'works' } )
+class AllPostsAPI(Resource):
+    def get(self):
+        query = client.query(kind='Post')
+        query.add_filter('public', '=', True)
+        return jsonify(list(query.fetch()))
+
+class PostAPI(Resource):
+    def get(self, title):
+        query = client.query(kind='Post')
+        query.add_filter('public', '=', True)
+        query.add_filter('title', '=', title)
+        result = list(query.fetch())
+        if len(result) == 0:
+            abort(404)
+        return jsonify(result[0])
--- src/server/server.py
+++ src/server/server.py
@@ -3,7 +3,10 @@ from flask import Flask, render_template
 from flask_restful import Api
 from api import posts
 
-app = Flask(__name__, static_folder="../static/dist", template_folder="../static/templates")
+app = Flask(
+        __name__,
+        static_folder="../static/dist",
+        template_folder="../static/templates")
 api = Api(app)
 
 @app.route("/")
@@ -14,7 +17,15 @@ def construction():
 def index():
     return render_template("index.html")
 
-api.add_resource(posts.PostsAPI, '/posts/api/v1.0/posts/<int:id>', endpoint='posts')
+# for the main page to retrive posts
+api.add_resource(
+        posts.PostAPI,
+        '/posts/api/v1.0/post/<string:title>',
+        endpoint='post')
+api.add_resource(
+        posts.AllPostsAPI,
+        '/posts/api/v1.0/posts',
+        endpoint='posts')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
