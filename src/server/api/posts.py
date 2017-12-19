ede18a833394deaa4e10470ad25c1949abbbf8c7
Angus Yip <ayip002@gmail.com>
2017-12-19 04:05:31 +0000

    Finish posts API v1.0

--- src/server/api/posts.py
+++ src/server/api/posts.py
@@ -5,18 +5,35 @@ from google.cloud import datastore
 
 client = datastore.Client()
 
+# Returns a datastore client query object of every post ordered by most recent
+def QueryHelper():
+    query = client.query(kind='Post', order=['-date'])
+    query.add_filter('public', '=', True)
+    return query
+
+# API entry point for all posts
 class AllPostsAPI(Resource):
     def get(self):
-        query = client.query(kind='Post', order=['-date'])
-        query.add_filter('public', '=', True)
+        query = QueryHelper()
         return jsonify(list(query.fetch()))
 
+# API entry point for a post given its title
 class PostAPI(Resource):
     def get(self, title):
-        query = client.query(kind='Post')
-        query.add_filter('public', '=', True)
+        query = QueryHelper()
         query.add_filter('title', '=', title)
         result = list(query.fetch())
         if len(result) == 0:
             abort(404)
         return jsonify(result[0])
+
+# API entry point for posts given a category
+class PostCategoryAPI(Resource):
+    def get(self, category):
+        query = QueryHelper()
+        fetched = query.fetch()
+        result = list(
+            post for post in fetched if category in post['categories'])
+        if len(result) == 0:
+            abort(404)
+        return jsonify(result)
--- src/server/server.py
+++ src/server/server.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template
 from flask_restful import Api
 from api import posts
+# Uncomment this for development in c9.io
+import os
 
 app = Flask(
         __name__,
@@ -17,15 +19,21 @@ def construction():
 def index():
     return render_template("index.html")
 
-# for the main page to retrive posts
+# APIs for retriving posts
 api.add_resource(
         posts.PostAPI,
         '/posts/api/v1.0/post/<string:title>',
         endpoint='post')
 api.add_resource(
         posts.AllPostsAPI,
-        '/posts/api/v1.0/posts',
-        endpoint='posts')
+        '/posts/api/v1.0/all',
+        endpoint='all')
+api.add_resource(
+        posts.PostCategoryAPI,
+        '/posts/api/v1.0/category/<string:category>',
+        endpoint='category')
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    #app.run(host='127.0.0.1', port=8080, debug=True)
+    # Uncomment this for development in c9.io
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)),debug=True)
