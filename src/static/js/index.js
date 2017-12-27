0b07d7bcc2e2b67031055406452b2d17f5d34b3b
Angus Yip <ayip002@gmail.com>
2017-12-27 02:57:05 -0800

    Sass config

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -2,11 +2,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import App from './components/App';
+import App from './components/App'
+import style from '../styles/main.scss'
 
 render(
     <BrowserRouter>
         <App />
     </BrowserRouter>,
     document.getElementById('container')
-);
+)
--- src/static/webpack.config.js
+++ src/static/webpack.config.js
@@ -1,40 +1,50 @@
 const { resolve } = require('path');
 const webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+  filename: "styles.css",
+  // disable: process.env.NODE_ENV === "development"
+});
 
 const config = {
-    entry: resolve(__dirname, 'js') + '/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: resolve(__dirname, 'dist'),
-        publicPath: '/',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.css']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test:/\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader',
-                })
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: 'file-loader'
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('styles.css'),
+  entry: resolve(__dirname, 'js') + '/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }],
+          // use style-loader in development
+          fallback: "style-loader"
+        })
+      }
     ]
+  },
+  plugins: [
+    extractSass
+  ]
 };
 
 
